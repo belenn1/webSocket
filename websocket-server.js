08b7ee2643ddef0eb822ed9b05daf554c3203ae0
kareendiaz <kareendiaz23@gmail.com>
2025-07-03 20:05:38 -0300

    Corregi los websocket clientsy server

--- websocket-server.js
+++ websocket-server.js
@@ -2,22 +2,22 @@ import { WebSocketServer } from "ws";
 
 // Crear un servidor WebSocket
 const server = new WebSocketServer({ port: 8080 });
+const clients = new Set();
 
 console.log("Servidor WebSocket ejecutándose en ws://localhost:8080");
 
 // Manejar nuevas conexiones
 server.on("connection", (socket) => {
+  clients.add(socket);
   console.log("Cliente conectado.");
 
-  // Enviar un mensaje al cliente
-  socket.send("¡Bienvenido al servidor WebSocket!");
-
-  // Escuchar mensajes del cliente
   socket.on("message", (message) => {
-    console.log(`Mensaje recibido del cliente: ${message}`);
-
-    // Responder al cliente
-    socket.send(`Servidor recibió: ${message}`);
+    clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        console.log(message);
+        client.send(message);
+      }
+    });
   });
 
   // Manejar cierre de conexión
